711b7199445240296d10b92b9b7aca7bf1b13aff
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-10-10 01:51:32 +0800

    Updates

---
+++ precache-manifest.3be5af11977ffca31d0c7e009f093e66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f257fe050e118aa07bfff548bf101c0c",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "a5af47eaf99b947f86f1",
+    "url": "/random-quote-machine/static/css/main.89e434c2.chunk.css"
+  },
+  {
+    "revision": "828c704ce711f0964864",
+    "url": "/random-quote-machine/static/js/2.f0dfa6fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.f0dfa6fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5af47eaf99b947f86f1",
+    "url": "/random-quote-machine/static/js/main.b4ec27d9.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
