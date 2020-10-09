1551bf7cf646723645cef17a237c5227e941ac0a
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-10-10 00:11:21 +0800

    Add and render sample quotes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
 
+const quotes = [
+  {
+    text:
+      "I am not a product of my circumstances. I am a product of my decisions.",
+    author: "Stephen Covey",
+  },
+  {
+    text:
+      "Remember that not getting what you want is sometimes a wonderful stroke of luck.",
+    author: "Dalai Lama",
+  },
+  {
+    text:
+      "Build your own dreams, or someone else will hire you to build theirs.",
+    author: "Farrah Gray",
+  },
+];
+
 function App() {
   return (
     <main id="quote-box">
-      <div id="text">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit,
-        inventore.
-      </div>
-      <div id="author">Lorem ipsum dolor sit amet.</div>
+      <div id="text">{quotes[0].text}</div>
+      <div id="author">{quotes[0].author}</div>
       <button id="new-quote">New Quote</button>
       <a href="#quote-box" id="tweet-quote">
         Tweet
