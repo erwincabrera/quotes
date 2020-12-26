c8638d58da023fefc6e4766fd452b98c1e179a30
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-12-26 12:36:17 +0800

    Adding index.html copy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 
-exports.default = function () {
+function css() {
   return gulp
     .src("./src/sass/**/*.scss")
     .pipe(sass())
     .on("error", sass.logError)
     .pipe(gulp.dest("./dist/"));
-};
+}
+
+function copy() {
+  return gulp.src("./src/index.html").pipe(gulp.dest("./dist/"));
+}
+
+exports.default = gulp.parallel(css, copy);
