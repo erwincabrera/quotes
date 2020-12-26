d3da51c1ffabc005b6ec8f3223a7e33a868bec3b
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2020-12-26 13:06:03 +0800

    Adding gulp sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
 
 sass.compiler = require("sass");
 
 function css() {
   return gulp
     .src("./src/sass/**/*.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass())
     .on("error", sass.logError)
+    .pipe(sourcemaps.write("./"))
     .pipe(gulp.dest("./dist/"));
 }
 
